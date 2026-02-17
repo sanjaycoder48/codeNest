50e76e58a8a2095570bd5f0d5b32313484e2c937
sanjay E <sanjaye0408@gmail.com>
2026-02-17 21:43:45 +0530

    navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,22 +1,29 @@
 function NavBar() {
     return (
-        <nav className="bg-white shadow-md px-0 py-4">
-            <div className="flex items-center justify-between max-w-7xl mx-auto ml-1">
+        <nav className="bg-white shadow-md px-0 py-4 flex">
+            <div className="flex items-center  max-w-7xl mx-auto ml-1">
                 <div className="flex m-auto ml-0">
                     <img src="" alt="codenest logo" className="m-1" />
                     <span className="text-2xl font-bold text-gray-950 bg-clip-text">CodeNest</span>
                 </div>
-                <div className="flex gap-8 ml-auto">
+            </div>
+            <div className="flex m-auto ml-0">
+            
+            <div className="flex gap-8 ml-auto items-center  ">
                     <a className="text-sm font-medium text-gray-950 hover:text-gray-800 hover:underline underline-offset-4  transition-colors duration-200" href="#">
                         Explore
                     </a>
-                    <a className="text-sm font-medium text-gray-950 hover:text-gray-800 transition-colors duration-200" href="#">
+                    <a className="text-sm font-medium text-gray-950 hover:text-gray-800 hover:underline underline-offset-4 transition-colors duration-200" href="#">
                         About
                     </a>
-                    <a className="text-sm font-medium text-gray-950 hover:text-gray-800 transition-colors duration-200" href="#">
+                    <a className="text-sm font-medium text-gray-950 hover:text-gray-800 hover:underline underline-offset-4 transition-colors duration-200" href="#">
                         Community
                     </a>
                 </div>
+                <div>
+                    
+                </div>
+                    
             </div>
         </nav>
     )
